0eff810acb07a0c91233abc1c98b1a4c47bd7e6f
dikhawk <dik_hawk@mail.ru>
2024-08-07 23:17:18 +0600

    fixed for windows platform

--- core/common/src/jvmMain/kotlin/com/dik/common/cmd/CmdRunner.jvm.kt
+++ core/common/src/jvmMain/kotlin/com/dik/common/cmd/CmdRunner.jvm.kt
@@ -1,5 +1,7 @@
 package com.dik.common.cmd
 
+import com.dik.common.Platform
+import com.dik.common.utils.platformName
 import com.dik.common.utils.readOutput
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
@@ -7,14 +9,14 @@ actual object KmpCmdRunner : CmdRunner {
     private val runnedProcesses = mutableListOf<Process>()
 
     override fun run(command: String) {
-        val fullCommand = listOf("sh", "-c", command)
+        val fullCommand = prepareCommand(command)
         val process = ProcessBuilder(fullCommand).start()
         println(process.readOutput())
         runnedProcesses.add(process)
     }
 
     override fun runAndWaitResult(command: String): String {
-        val fullCommand = listOf("sh", "-c", command)
+        val fullCommand = prepareCommand(command)
         val process = ProcessBuilder(fullCommand).start()
 
         runnedProcesses.add(process)
@@ -25,5 +27,13 @@ actual object KmpCmdRunner : CmdRunner {
 
     override fun stopRunnedProcesses() {
         runnedProcesses.forEach { it.destroy() }
+    }
+
+    private fun prepareCommand(command: String): List<String> {
+        return when(platformName()) {
+            Platform.LINUX -> listOf("sh", "-c", command)
+            Platform.WINDOWS -> listOf("cmd", "/c", command)
+            else -> throw UnsupportedOperationException("Unsupported platform")
+        }
     }
 }
