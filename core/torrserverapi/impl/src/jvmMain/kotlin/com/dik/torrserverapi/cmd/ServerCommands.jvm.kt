36b6e057f2650cb79d3818d4643ebeaa33180514
Dik <dik_hawk@mail.ru>
2024-08-12 18:16:15 +0600

    removed unused code, added start server for Windows

--- core/torrserverapi/impl/src/jvmMain/kotlin/com/dik/torrserverapi/cmd/ServerCommands.jvm.kt
+++ core/torrserverapi/impl/src/jvmMain/kotlin/com/dik/torrserverapi/cmd/ServerCommands.jvm.kt
@@ -1,30 +1,20 @@
 package com.dik.torrserverapi.cmd
 
 import com.dik.common.Platform
+import com.dik.common.cmd.KmpCmdRunner
 import com.dik.common.utils.platformName
 import com.dik.common.utils.readOutput
-import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object KmpServerCommands : ServerCommands {
     override fun startServer(pathToServerFile: String) {
-/*        val makeExecutableCommand = "chmod +x $pathToServerFile"
-        val startServerCommand = "cd $pathToServer && ./$serverName"
-        val fullCommand = arrayOf("sh", "-c", "$makeExecutableCommand && $startServerCommand")
-        val process = Runtime.getRuntime().exec(fullCommand)
-
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        var line: String? = reader.readLine()
-        while (line != null) {
-            println(line)
-            line = reader.readLine()
-        }*/
-
         when (platformName()) {
             Platform.LINUX -> startServerOnLinux(pathToServerFile)
-            else -> { TODO("Not yet implemented") }
+            Platform.WINDOWS -> startServerOnWindows(pathToServerFile)
+            else -> {
+                TODO("Not yet implemented")
+            }
         }
     }
 
@@ -37,10 +27,15 @@ actual object KmpServerCommands : ServerCommands {
         val makeExecutableCommand = "chmod +x $pathToServerFile"
         val startServerCommand = "cd ${serverFile.parent} && ./${serverFile.name}"
 
-        val c = listOf("sh", "-c", "$makeExecutableCommand && $startServerCommand").joinToString(" ")
         val startServerProcess = ProcessBuilder(
             listOf("sh", "-c", "$makeExecutableCommand && $startServerCommand")
         ).start()
         println(startServerProcess.readOutput())
+    }
+
+    private fun startServerOnWindows(pathToServerFile: String) {
+        if (pathToServerFile.isEmpty()) return
+
+        KmpCmdRunner.run(pathToServerFile)
     }
 }
