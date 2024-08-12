694d0dd27d133cf70f5a1e72c5b42aeaa0208fc9
Dik <dik_hawk@mail.ru>
2024-08-12 17:41:34 +0600

    optimized imports

--- core/appsettings/impl/src/commonMain/kotlin/com/dik/appsettings/impl/di/AppSettingModule.kt
+++ core/appsettings/impl/src/commonMain/kotlin/com/dik/appsettings/impl/di/AppSettingModule.kt
@@ -1,8 +1,5 @@
 package com.dik.appsettings.impl.di
 
-import com.russhwolf.settings.PreferencesSettings
-import com.russhwolf.settings.Settings
 import org.koin.core.module.Module
-import org.koin.dsl.module
 
 expect fun appSettingModule(dependencies: AppSettingsDependencies): Module
