b79deed3a06db7ced9908b04105d1b7e9de15ce3
dikhawk <dik_hawk@mail.ru>
2024-08-16 21:42:15 +0600

    startServer(): if server started return imediatly Success; stopServer(): a waiting stop server, after that return result

--- core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/server/TorrserverCommandsImpl.kt
+++ core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/server/TorrserverCommandsImpl.kt
@@ -1,30 +1,44 @@
 package com.dik.torrserverapi.server
 
+import com.dik.common.AppDispatchers
 import com.dik.common.Platform
 import com.dik.common.Result
 import com.dik.common.ResultProgress
 import com.dik.common.utils.platformName
+import com.dik.common.utils.repeatIfError
 import com.dik.torrserverapi.TorrserverError
 import com.dik.torrserverapi.cmd.ServerCommands
 import com.dik.torrserverapi.model.TorrserverFile
 import com.dik.torrserverapi.utils.defaultDirectory
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import okio.FileNotFoundException
+import okio.FileSystem
 import okio.Path
+import okio.Path.Companion.toPath
 
 internal class TorrserverCommandsImpl(
     private val serverCommands: ServerCommands,
     private val torrserverStuffApi: TorrserverStuffApi,
-    private val installTorrserver: InstallTorrserver
+    private val installTorrserver: InstallTorrserver,
+    private val appDispatchers: AppDispatchers
 ) : TorrserverCommands {
 
     override suspend fun installServer(): Flow<ResultProgress<TorrserverFile, TorrserverError>> =
-        installTorrserver.start(pathToServerFile())
+        withContext(appDispatchers.defaultDispatcher()) { installTorrserver.start(pathToServerFile()) }
 
     override suspend fun startServer(): Result<Unit, TorrserverError> {
         try {
-            if (torrserverStuffApi.echo() is Result.Success) return Result.Success(Unit)
+            val echoResult = withContext(appDispatchers.ioDispatcher()) {
+                torrserverStuffApi.echo()
+            }
+            if (echoResult is Result.Success) return Result.Success(Unit)
 
-            serverCommands.startServer(pathToServerFile())
+            withContext(appDispatchers.defaultDispatcher()) {
+                serverCommands.startServer(pathToServerFile())
+            }
 
             return Result.Success(Unit)
         } catch (e: Exception) {
@@ -32,8 +46,7 @@ internal class TorrserverCommandsImpl(
         }
     }
 
-    private fun pathToServerFile() =
-        defaultDirectory() + Path.DIRECTORY_SEPARATOR + serverName()
+    private fun pathToServerFile() = defaultDirectory() + Path.DIRECTORY_SEPARATOR + serverName()
 
     private fun serverName(): String {
         var serverName = "TorrServer"
@@ -44,6 +57,74 @@ internal class TorrserverCommandsImpl(
     }
 
     override suspend fun stopServer(): Result<Unit, TorrserverError> {
-        return torrserverStuffApi.stopServer()
+        val result = withContext(appDispatchers.ioDispatcher()) {
+            torrserverStuffApi.stopServer()
+        }
+
+        //A waiting when server stopped
+        var tries = 3
+        for (t in 1..tries) {
+            val echoResult = withContext(appDispatchers.ioDispatcher()) { torrserverStuffApi.echo() }
+            if (echoResult is Result.Error) break
+
+            delay(1000)
+        }
+
+        return result
+    }
+
+    override suspend fun isServerInstalled(): Result<Boolean, TorrserverError> {
+        val isServerInstalled = withContext(appDispatchers.ioDispatcher()) {
+            try {
+                val path = pathToServerFile().toPath()
+                val fileSize = FileSystem.SYSTEM.metadata(path).size ?: 0L
+                val isfileExist = FileSystem.SYSTEM.exists(path)
+
+                fileSize > 0 && isfileExist
+            } catch (e: FileNotFoundException) {
+                false
+            }
+        }
+
+        return Result.Success(isServerInstalled)
+    }
+
+    override suspend fun isServerStarted(): Result<Boolean, TorrserverError> {
+        try {
+            val echoResult = withContext(appDispatchers.ioDispatcher()) {
+                torrserverStuffApi.echo()
+            }
+            val isStarted = echoResult is Result.Success
+
+            return Result.Success(isStarted)
+        } catch (e: Exception) {
+            return Result.Error(TorrserverError.Common.Unknown(e.message ?: ""))
+        }
+    }
+
+    override suspend fun isAvailableNewVersion(): Result<Boolean, TorrserverError> {
+        try {
+            val serverStatusResult = withContext(appDispatchers.ioDispatcher()) {
+                torrserverStuffApi.echo()
+            }
+
+            if (serverStatusResult is Result.Error) return Result.Error(serverStatusResult.error)
+
+            val latestServerVersionResult = torrserverStuffApi.checkLatestRelease()
+
+            if (latestServerVersionResult is Result.Error) return Result.Error(latestServerVersionResult.error)
+
+            if (serverStatusResult is Result.Success && latestServerVersionResult is Result.Success) {
+                val localServerVersion = serverStatusResult.data
+                val latestServerVersion = latestServerVersionResult.data.tagName
+                val isAvailableNewVersion = localServerVersion != latestServerVersion
+
+                return Result.Success(isAvailableNewVersion)
+            }
+
+            return Result.Error(TorrserverError.Common.Unknown("Can't check updates"))
+        } catch (e: Exception) {
+            return Result.Error(TorrserverError.Common.Unknown(e.message ?: ""))
+        }
     }
 }
