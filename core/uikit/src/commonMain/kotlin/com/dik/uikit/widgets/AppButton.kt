17c07e121f0d327f19911c8e1ad2cfe4cc2b93ba
dikhawk <dik_hawk@mail.ru>
2024-08-16 21:43:30 +0600

    Added button

---
+++ core/uikit/src/commonMain/kotlin/com/dik/uikit/widgets/AppButton.kt
@@ -0,0 +1,12 @@
+package com.dik.uikit.widgets
+
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppButton(text: String, modifier: Modifier = Modifier, enabled : Boolean = true, onClick: () -> Unit) {
+    Button(onClick = onClick, enabled = enabled) {
+        AppNormalText(text = text)
+    }
+}
