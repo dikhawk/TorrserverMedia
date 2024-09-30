64cf26a4a3f845f0f1e8ca39856ecd2b0b6d02de
dikhawk <dik_hawk@mail.ru>
2024-09-30 17:42:54 +0600

    removed unused imports

--- features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/screens/main/MainUi.kt
+++ features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/screens/main/MainUi.kt
@@ -7,12 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
