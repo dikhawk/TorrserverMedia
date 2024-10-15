dd99ee535b98974242b327be1d26a199a7f7a91f
dikhawk <dik_hawk@mail.ru>
2024-10-15 09:59:48 +0600

    refactoring

--- composeApp/src/commonMain/kotlin/com/dik/torrservermedia/di/KoinModules.kt
+++ composeApp/src/commonMain/kotlin/com/dik/torrservermedia/di/KoinModules.kt
@@ -1,37 +1,46 @@
 package com.dik.torrservermedia.di
 
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.koin.core.Koin
+import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.koinApplication
 
 object KoinModules {
 
-    private var koinConfig: KoinAppDeclaration? = null
+    private val mutex = Mutex()
 
-    val koin: Koin by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
-        koinApplication {
-            koinConfig?.invoke(this)
-            modules(
-                appModule,
-                platformModule(),
-                appSettingsModule,
-                featuresModule,
-                torrserverModule,
-                theMovieDbApiModule
-            )
-        }.koin
+    var koin: Koin? = null
+        private set
+
+    fun init(config: KoinAppDeclaration? = null) {
+        if (koin == null) {
+            runBlocking {
+                mutex.withLock {
+                    startKoin { }
+                    koin = koinApplication {
+                        config?.invoke(this)
+                        appModules()
+                    }.koin
+                }
+            }
+        }
     }
 
-    fun init(config: KoinAppDeclaration? = null): Koin {
-        koinConfig = config
-
-        startKoin { }
-
-        return koin
+    private fun KoinApplication.appModules() {
+        modules(
+            appModule,
+            appSettingsModule,
+            featuresModule,
+            torrserverModule,
+            theMovieDbApiModule
+        )
     }
 }
 
 internal inline fun <reified T> inject(): T {
-    return KoinModules.koin.get()
+    return KoinModules.koin!!.get()
 }
