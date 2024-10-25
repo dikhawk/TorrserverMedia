ef2f93faa7ae18035dcc4a239ac66f7eafea2bc0
dikhawk <dik_hawk@mail.ru>
2024-10-25 13:30:22 +0600

    call startServer() on starting app

--- composeApp/src/androidMain/kotlin/com/dik/torrservermedia/App.android.kt
+++ composeApp/src/androidMain/kotlin/com/dik/torrservermedia/App.android.kt
@@ -1,15 +1,25 @@
 package com.dik.torrservermedia
 
 import android.app.Application
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.lifecycle.lifecycleScope
 import com.arkivanov.decompose.defaultComponentContext
+import com.dik.common.utils.successResult
+import com.dik.torrserverapi.di.TorrserverApi
+import com.dik.torrserverapi.server.TorrserverCommands
 import com.dik.torrservermedia.di.KoinModules
 import com.dik.torrservermedia.di.inject
 import com.dik.torrservermedia.nanigation.DefaultRootComponent
 import com.dik.uikit.theme.AppTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 
 class AndroidApp : Application() {
@@ -21,28 +31,54 @@ class AndroidApp : Application() {
         super.onCreate()
         INSTANCE = this
 
-
         KoinModules.init {
             androidContext(INSTANCE)
         }
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 class AppActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val torrserverApi: TorrserverApi = inject()
+        val commands: TorrserverCommands = torrserverApi.torrserverCommands()
         val root = DefaultRootComponent(
             componentContext = defaultComponentContext(),
             featureTorrentListApi = inject(),
             featureSettingsApi = inject()
         )
 
+        lifecycleScope.launch {
+            val isInstalledServer = commands.isServerInstalled().successResult() ?: false
+            if (isInstalledServer) commands.startServer()
+        }
+
         enableEdgeToEdge()
         setContent {
+            val permissions = remember {
+                mutableListOf<String>().apply {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        add(android.Manifest.permission.POST_NOTIFICATIONS)
+                    }
+                }
+            }
+            val notificationPermission = rememberMultiplePermissionsState(permissions)
+
             AppTheme {
                 RootUi(component = root)
             }
+
+            LaunchedEffect(Unit) {
+                if (!notificationPermission.allPermissionsGranted)
+                    notificationPermission.launchMultiplePermissionRequest()
+            }
         }
+    }
+
+    override fun finish() {
+        super.finish()
     }
 }
--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -1,7 +1,6 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -9,6 +8,8 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.dik.common.AppDispatchers
+import com.dik.common.utils.successResult
 import com.dik.torrserverapi.di.TorrserverApi
 import com.dik.torrservermedia.RootUi
 import com.dik.torrservermedia.di.KoinModules
@@ -16,6 +17,9 @@ import com.dik.torrservermedia.di.inject
 import com.dik.torrservermedia.nanigation.DefaultRootComponent
 import com.dik.torrservermedia.utils.runOnUiThread
 import com.dik.uikit.theme.AppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.compose.resources.stringResource
 import torrservermedia.composeapp.generated.resources.Res
@@ -27,6 +31,9 @@ fun main() {
     val lifecycle = LifecycleRegistry()
     KoinModules.init()
     val torrServerApi: TorrserverApi = inject()
+    val dispatchers: AppDispatchers = inject()
+    val commands = torrServerApi.torrserverCommands()
+    val scope = CoroutineScope(dispatchers.defaultDispatcher() + SupervisorJob())
     val root = runOnUiThread {
         DefaultRootComponent(
             componentContext = DefaultComponentContext(lifecycle = lifecycle),
@@ -35,12 +42,17 @@ fun main() {
         )
     }
 
+    scope.launch {
+        val isInstalledServer = commands.isServerInstalled().successResult() ?: false
+        if (isInstalledServer) commands.startServer()
+    }
+
     application {
         Window(
             title = stringResource(Res.string.app_name),
             state = rememberWindowState(width = 1000.dp, height = 600.dp),
             onCloseRequest = {
-                runBlocking { torrServerApi.torrserverCommands().stopServer() }
+                runBlocking { commands.stopServer() }
                 exitApplication()
             }
         ) {
