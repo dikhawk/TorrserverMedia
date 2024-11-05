91c40ff9a52a406199ccafd977e30da429dbd12c
dikhawk <dik_hawk@mail.ru>
2024-11-05 23:32:56 +0600

    removed expect and added implementation

--- core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/utils/FileUtils.kt
+++ core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/utils/FileUtils.kt
@@ -1,10 +1,5 @@
 package com.dik.torrserverapi.utils
 
-import okio.FileSystem
 import okio.Path.Companion.toPath
 
-fun String.fileToByteArray(): ByteArray {
-    return FileSystem.SYSTEM.read(this.toPath()) { readByteString().toByteArray() }
-}
-
-expect fun defaultDirectory(): String
+fun fileToByteArray(fileToPath: String): ByteArray = fileToPath.toPath().nameBytes.toByteArray()
