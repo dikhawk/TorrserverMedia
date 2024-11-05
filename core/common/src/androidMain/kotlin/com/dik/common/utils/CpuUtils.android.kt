9f794600c7c447eda7bf1e340482e75529a01592
dikhawk <dik_hawk@mail.ru>
2024-11-05 22:43:16 +0600

    fixed invalid parse cpu arch

--- core/common/src/androidMain/kotlin/com/dik/common/utils/CpuUtils.android.kt
+++ core/common/src/androidMain/kotlin/com/dik/common/utils/CpuUtils.android.kt
@@ -3,5 +3,5 @@ package com.dik.common.utils
 import android.os.Build
 
 actual fun cpuArch(): String {
-    return Build.SUPPORTED_ABIS.firstOrNull() ?: "unknown"
+    return Build.SUPPORTED_ABIS.firstOrNull()?.split("-")?.firstOrNull() ?: "unknown"
 }
