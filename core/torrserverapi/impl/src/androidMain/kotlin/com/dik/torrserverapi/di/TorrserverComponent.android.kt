e7db07d6caa944fd901fa2130abee13b60cd135b
dikhawk <dik_hawk@mail.ru>
2024-11-05 22:10:00 +0600

    added torserverComponent for multiplatform

---
+++ core/torrserverapi/impl/src/androidMain/kotlin/com/dik/torrserverapi/di/TorrserverComponent.android.kt
@@ -0,0 +1,24 @@
+package com.dik.torrserverapi.di
+
+import com.dik.torrserverapi.model.TorrserverServiceManager
+import com.dik.torrserverapi.server.MagnetApi
+import com.dik.torrserverapi.server.ServerSettingsApi
+import com.dik.torrserverapi.server.TorrentApi
+import com.dik.torrserverapi.server.TorrserverCommands
+import com.dik.torrserverapi.server.TorrserverStuffApi
+
+internal actual fun torrserverComponent(
+    dependencies: TorrserverDependencies
+) = object : TorrserverComponent() {
+    init {
+        KoinModules.init(dependencies)
+    }
+
+    override fun magnetApi(): MagnetApi = inject()
+    override fun torrentApi(): TorrentApi = inject()
+    override fun torrserverStuffApi(): TorrserverStuffApi = inject()
+    override fun torrserverCommands(): TorrserverCommands = inject()
+    override fun serverSettingsApi(): ServerSettingsApi = inject()
+    override fun torrserverServiceManager(): TorrserverServiceManager = inject()
+
+}
--- core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/di/TorrserverComponent.kt
+++ core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/di/TorrserverComponent.kt
@@ -1,6 +1,5 @@
 package com.dik.torrserverapi.di
 
-import com.dik.common.AppDispatchers
 import com.dik.torrserverapi.server.MagnetApi
 import com.dik.torrserverapi.server.ServerSettingsApi
 import com.dik.torrserverapi.server.TorrserverCommands
@@ -21,17 +20,7 @@ abstract class TorrserverComponent : TorrserverApi {
                 runBlocking {
                     mutex.withLock {
                         if (torrserverComponent == null) {
-                            torrserverComponent = object : TorrserverComponent() {
-                                init {
-                                    KoinModules.init(dependencies)
-                                }
-
-                                override fun magnetApi(): MagnetApi = inject()
-                                override fun torrentApi(): TorrentApi = inject()
-                                override fun torrserverStuffApi(): TorrserverStuffApi = inject()
-                                override fun torrserverCommands(): TorrserverCommands = inject()
-                                override fun serverSettingsApi(): ServerSettingsApi = inject()
-                            }
+                            torrserverComponent = torrserverComponent(dependencies)
                         }
                     }
                 }
@@ -41,3 +30,5 @@ abstract class TorrserverComponent : TorrserverApi {
         }
     }
 }
+
+internal expect fun torrserverComponent(dependencies: TorrserverDependencies): TorrserverComponent
