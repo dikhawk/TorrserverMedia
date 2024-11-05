2ff806230f072a82c69b72f509addd687ddc3482
dikhawk <dik_hawk@mail.ru>
2024-11-05 22:36:15 +0600

    added implementation

--- core/common/src/androidMain/kotlin/com/dik/common/cmd/CmdRunner.android.kt
+++ core/common/src/androidMain/kotlin/com/dik/common/cmd/CmdRunner.android.kt
@@ -1,9 +1,18 @@
 package com.dik.common.cmd
 
+import com.topjohnwu.superuser.Shell
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object KmpCmdRunner : CmdRunner {
+
+    private val shellJob: Shell.Job by lazy {
+        Shell.Builder.create()
+            .setFlags(Shell.FLAG_NON_ROOT_SHELL)
+            .build().newJob()
+    }
+
     override fun run(command: String) {
-        TODO("Not yet implemented")
+        shellJob.add(command).enqueue()
     }
 
     override fun runAndWaitResult(command: String): String {
