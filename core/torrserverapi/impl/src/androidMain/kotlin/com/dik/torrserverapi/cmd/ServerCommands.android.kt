1b1012c1ce7826788bcc309d562eb1c6b514fb37
dikhawk <dik_hawk@mail.ru>
2024-11-05 23:34:46 +0600

    removed server commands

--- core/torrserverapi/impl/src/androidMain/kotlin/com/dik/torrserverapi/cmd/ServerCommands.android.kt
+++
@@ -1,8 +0,0 @@
-package com.dik.torrserverapi.cmd
-
-@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-actual object KmpServerCommands : ServerCommands {
-    override fun startServer(pathToServerFile: String) {
-        TODO("Not yet implemented")
-    }
-}
--- core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/cmd/ServerCommands.kt
+++
@@ -1,8 +0,0 @@
-package com.dik.torrserverapi.cmd
-
-interface ServerCommands {
-    fun startServer(pathToServerFile: String)
-}
-
-@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-expect object KmpServerCommands: ServerCommands
