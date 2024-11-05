94e5447c50319d552dbbed737f93103208ed6551
dikhawk <dik_hawk@mail.ru>
2024-11-05 22:26:40 +0600

    added logs

--- core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/server/BackupFile.kt
+++ core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/server/BackupFile.kt
@@ -1,5 +1,6 @@
 package com.dik.torrserverapi.server
 
+import co.touchlab.kermit.Logger
 import com.dik.common.Result
 import com.dik.torrserverapi.TorrserverError
 import okio.FileSystem
@@ -7,17 +8,26 @@ import okio.Path.Companion.toPath
 
 
 internal class BackupFile {
-    operator fun invoke(pathToFile: String, pathToBackupFile: String): Result<Unit, TorrserverError> {
+    val tag = "BackupFile:"
+
+    operator fun invoke(
+        pathToFile: String,
+        pathToBackupFile: String
+    ): Result<Unit, TorrserverError> {
         val fileSystem = FileSystem.SYSTEM
         val file = pathToFile.toPath()
         val backUpFile = pathToBackupFile.toPath()
+        Logger.i("$tag started backup for file: $pathToFile")
 
-        if (!fileSystem.exists(file))
+        if (!fileSystem.exists(file)) {
+            Logger.e("$tag File not exist: $pathToFile")
             return Result.Error(TorrserverError.Server.FileNotExist("File not exist: $pathToFile"))
+        }
 
         fileSystem.delete(backUpFile)
         fileSystem.copy(source = file, target = backUpFile)
 
+        Logger.i("$tag Success result. Created backup for file: $pathToFile,  saved to $pathToBackupFile")
         return Result.Success(Unit)
     }
 }
--- core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/server/RestoreServerFromBackUp.kt
+++ core/torrserverapi/impl/src/commonMain/kotlin/com/dik/torrserverapi/server/RestoreServerFromBackUp.kt
@@ -1,21 +1,30 @@
 package com.dik.torrserverapi.server
 
+import co.touchlab.kermit.Logger
 import com.dik.common.Result
 import com.dik.torrserverapi.TorrserverError
 import okio.FileSystem
 import okio.Path.Companion.toPath
 
 class RestoreServerFromBackUp {
+    private val tag = "RestoreServerFromBackUp:"
+
     operator fun invoke(pathToBackupFile: String, pathToFile: String): Result<Unit, TorrserverError> {
+        Logger.i("$tag start restore server $pathToFile from backup file $pathToBackupFile")
         val fileSystem = FileSystem.SYSTEM
         val file = pathToFile.toPath()
         val backUpFile = pathToBackupFile.toPath()
 
-        if (!fileSystem.exists(file))
-            return Result.Error(TorrserverError.Server.FileNotExist("File not exist: $pathToFile"))
+        if (!fileSystem.exists(backUpFile)) {
+            Logger.e("$tag backup file not exist $pathToBackupFile")
+            return Result.Error(TorrserverError.Server.FileNotExist("Backup file not exist: $pathToBackupFile"))
+        }
 
-        fileSystem.delete(file)
+        if (fileSystem.exists(file)) fileSystem.delete(file)
+
         fileSystem.copy(source = backUpFile, target = file)
+
+        Logger.i("$tag file restored from backup successfully")
 
         return Result.Success(Unit)
     }
