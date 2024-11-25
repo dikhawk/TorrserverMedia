6d87fc20a954c7236f9db865c6fade1a1c5caa7e
dikhawk <dik_hawk@mail.ru>
2024-11-25 18:34:30 +0600

    added platform realization

---
+++ composeApp/src/jvmMain/kotlin/com/dik/torrservermedia/di/FeaturesModule.jvm.kt
@@ -0,0 +1,16 @@
+package com.dik.torrservermedia.di
+
+import com.dik.appsettings.api.model.AppSettings
+import com.dik.common.AppDispatchers
+import com.dik.settings.SettingsFeatureApi
+import com.dik.themoviedb.di.TheMovieDbApi
+import com.dik.torrentlist.di.TorrentListDependencies
+import com.dik.torrserverapi.di.TorrserverApi
+
+internal actual fun torrentListDependencies() = object : TorrentListDependencies {
+    override fun torrServerApi(): TorrserverApi = inject()
+    override fun dispatchers(): AppDispatchers = inject()
+    override fun settingsFeatureApi(): SettingsFeatureApi = inject()
+    override fun appSettings(): AppSettings = inject()
+    override fun theMovieDbApi(): TheMovieDbApi = inject()
+}
