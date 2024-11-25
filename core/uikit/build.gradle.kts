8f41e438b7ae52ac136cd4e543551eb17c836820
dikhawk <dik_hawk@mail.ru>
2024-11-25 18:30:07 +0600

    added min sdk

--- core/uikit/build.gradle.kts
+++ core/uikit/build.gradle.kts
@@ -11,6 +11,10 @@ android {
     namespace = "com.dik.uikit"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
