9a989a328177cea2ae4a625bd19c4df5efa39cfa
dikhawk <dik_hawk@mail.ru>
2024-11-25 18:28:43 +0600

    added implementation for android

--- core/common/src/androidMain/kotlin/com/dik/common/player/PlayersCommands.android.kt
+++ core/common/src/androidMain/kotlin/com/dik/common/player/PlayersCommands.android.kt
@@ -1,16 +1,36 @@
 package com.dik.common.player
 
-actual fun platformPlayersCommands(): PlayersCommands {
-    return AndroidPlayersCommands()
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import java.net.URLConnection
+
+actual fun platformPlayersCommands(deps: PlatformPlayersDependencies): PlayersCommands {
+    return AndroidPlayersCommands(deps)
 }
 
-class AndroidPlayersCommands : PlayersCommands {
+class AndroidPlayersCommands(private val deps: PlatformPlayersDependencies) : PlayersCommands {
 
     override suspend fun playFileInDefaultPlayer(fileName: String, fileUrl: String) {
-        TODO("Not yet implemented")
+        deps.context().openInPlayer(fileName, fileUrl)
     }
 
     override suspend fun playFile(fileName: String, fileUrl: String, player: Player) {
-        TODO("Not yet implemented")
+        deps.context().openInPlayer(fileName, fileUrl)
     }
+
+    private fun Context.openInPlayer(fileName: String, fileUrl: String) {
+        val mimeType = URLConnection.guessContentTypeFromName(fileName)
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(Uri.parse(fileUrl), mimeType)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+        startActivity(intent)
+    }
+}
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual interface PlatformPlayersDependencies {
+    fun context(): Context
 }
