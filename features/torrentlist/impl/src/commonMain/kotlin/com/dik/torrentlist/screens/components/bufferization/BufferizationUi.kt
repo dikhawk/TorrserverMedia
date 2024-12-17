2bddef5bd0c52e8f356cc65aaf1d470506d10325
dikhawk <dik_hawk@mail.ru>
2024-12-17 20:44:30 +0600

    fixed dialog oversize

--- features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/screens/components/bufferization/BufferizationUi.kt
+++ features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/screens/components/bufferization/BufferizationUi.kt
@@ -1,12 +1,17 @@
 package com.dik.torrentlist.screens.components.bufferization
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
@@ -28,40 +33,58 @@ import torrservermedia.features.torrentlist.impl.generated.resources.main_buffer
 @Composable
 internal fun BufferizationUi(component: BufferizationComponent, modifier: Modifier = Modifier) {
     val uiState = component.uiState.collectAsState()
-
     Dialog(onDismissRequest = {}) {
-        Card(modifier = modifier) {
-            Column(modifier = Modifier.padding(top = 8.dp, start = 8.dp, end = 8.dp)) {
-                AppNormalText(text = uiState.value.fileName)
-                if(uiState.value.title.isNotEmpty())
-                    AppNormalBoldText(uiState.value.title)
-                if(uiState.value.titleSecond.isNotEmpty())
-                    AppNormaBoldlItalicText(uiState.value.titleSecond)
-                Spacer(modifier = Modifier.height(16.dp))
-                Row {
-                    AppNormalItalicText(text = stringResource(Res.string.main_bufferization_downloading_speed))
-                    Spacer(modifier = Modifier.width(8.dp))
-                    AppNormalBoldText(text = uiState.value.downloadSpeed)
-                    Spacer(modifier = Modifier.weight(1f))
-                    AppNormalBoldText(text = uiState.value.fileSize)
-                }
-                Spacer(modifier = Modifier.height(8.dp))
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    LinearProgressIndicator(
-                        progress = { uiState.value.downloadProgress },
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    AppNormalBoldText(text = uiState.value.downloadProgressText)
-                    if(uiState.value.overview.isNotEmpty())
-                        AppNormalItalicText(text = uiState.value.overview)
+        BoxWithConstraints {
+            val maxHeight = maxHeight
 
-                }
-                Spacer(modifier = Modifier.height(8.dp))
-                Row {
-                    Spacer(modifier = Modifier.weight(1f))
-                    AppTextButton(
-                        text = stringResource(Res.string.main_bufferization_button_cancel),
-                        onClick = { component.onClickCancel() })
+            Card(modifier = modifier) {
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    Column(
+                        modifier = Modifier.heightIn(max = maxHeight - 100.dp)
+                            .align(Alignment.TopCenter)
+                            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+                    ) {
+                        AppNormalText(text = uiState.value.fileName)
+                        if (uiState.value.title.isNotEmpty())
+                            AppNormalBoldText(uiState.value.title)
+                        if (uiState.value.titleSecond.isNotEmpty())
+                            AppNormaBoldlItalicText(uiState.value.titleSecond)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Row {
+                            AppNormalItalicText(text = stringResource(Res.string.main_bufferization_downloading_speed))
+                            Spacer(modifier = Modifier.width(8.dp))
+                            AppNormalBoldText(text = uiState.value.downloadSpeed)
+                            Spacer(modifier = Modifier.weight(1f))
+                            AppNormalBoldText(text = uiState.value.fileSize)
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            LinearProgressIndicator(
+                                progress = { uiState.value.downloadProgress },
+                                modifier = Modifier.fillMaxWidth(),
+                            )
+                            AppNormalBoldText(text = uiState.value.downloadProgressText)
+                        }
+                        val bottomPadding = 56.dp
+
+                        if (uiState.value.overview.isNotEmpty()) {
+                            Column(
+                                modifier = Modifier.fillMaxWidth().padding(bottom = bottomPadding)
+                                    .verticalScroll(rememberScrollState())
+                            ) {
+                                AppNormalItalicText(text = uiState.value.overview)
+                            }
+                        } else {
+                            Spacer(modifier = Modifier.height(bottomPadding))
+                        }
+                    }
+
+                    Row(modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter)) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        AppTextButton(
+                            text = stringResource(Res.string.main_bufferization_button_cancel),
+                            onClick = { component.onClickCancel() })
+                    }
                 }
             }
         }
