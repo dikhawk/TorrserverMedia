3f34c37e7e0731dcaa48bebf8e868cf143bcf3ef
dikhawk <dik_hawk@mail.ru>
2025-01-29 18:20:58 +0600

    changed config directory

--- core/torrserverapi/impl/src/jvmMain/kotlin/com/dik/torrserverapi/server/TorrserverConfig.jvm.kt
+++ core/torrserverapi/impl/src/jvmMain/kotlin/com/dik/torrserverapi/server/TorrserverConfig.jvm.kt
@@ -3,21 +3,22 @@ package com.dik.torrserverapi.server
 import com.dik.common.Platform
 import com.dik.common.utils.platformName
 import java.io.File
+import java.nio.file.Paths
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 internal actual object TorrserverConfig : ServerConfig {
 
-    private val defaultDirectory = System.getProperty("user.dir")
+    private val configDirectory = getConfigDirectory() + File.separator + "TorrServerMedia"
 
     override val torrserverHost: String = "http://127.0.0.1:8090"
 
     override val pathToServerFile: String = when (platformName()) {
-        Platform.LINUX -> defaultDirectory + File.separator + torrServerFileName()
-        Platform.WINDOWS -> defaultDirectory + File.separator + torrServerFileName()
+        Platform.LINUX -> configDirectory + File.separator + torrServerFileName()
+        Platform.WINDOWS -> configDirectory + File.separator + torrServerFileName()
         else -> throw UnsupportedOperationException("Platform not supported ${platformName()}")
     }
 
-    override val pathToBackupServerFile = defaultDirectory + File.separator + backUpTorrServerFileName()
+    override val pathToBackupServerFile = configDirectory + File.separator + backUpTorrServerFileName()
 
     override val torrServerFileName: String = torrServerFileName()
 
@@ -34,4 +35,16 @@ internal actual object TorrserverConfig : ServerConfig {
     override val backUpTorrServerFileName = backUpTorrServerFileName()
 
     private fun backUpTorrServerFileName() = "old_${torrServerFileName}"
+
+    private fun getConfigDirectory(): String = when (platformName()) {
+        Platform.LINUX -> {
+            val xdgConfigHome = System.getenv("XDG_CONFIG_HOME")
+            Paths.get(xdgConfigHome ?: "${System.getProperty("user.home")}/.config").toString()
+        }
+        Platform.WINDOWS -> {
+            val localAppData = System.getenv("LOCALAPPDATA")
+            Paths.get(localAppData).resolve("MyApp").toString()
+        }
+        else -> throw UnsupportedOperationException("Platform not supported ${platformName()}")
+    }
 }
