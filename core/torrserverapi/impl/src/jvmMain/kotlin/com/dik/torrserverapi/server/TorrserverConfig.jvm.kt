30f0b9b273c2ba90c2ea20619dbc24b6122334f9
dikhawk <dik_hawk@mail.ru>
2025-01-29 19:29:17 +0600

    added config directory for Mac

--- core/torrserverapi/impl/src/jvmMain/kotlin/com/dik/torrserverapi/server/TorrserverConfig.jvm.kt
+++ core/torrserverapi/impl/src/jvmMain/kotlin/com/dik/torrserverapi/server/TorrserverConfig.jvm.kt
@@ -3,7 +3,6 @@ package com.dik.torrserverapi.server
 import com.dik.common.Platform
 import com.dik.common.utils.platformName
 import java.io.File
-import java.nio.file.Paths
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 internal actual object TorrserverConfig : ServerConfig {
@@ -15,6 +14,7 @@ internal actual object TorrserverConfig : ServerConfig {
     override val pathToServerFile: String = when (platformName()) {
         Platform.LINUX -> configDirectory + File.separator + torrServerFileName()
         Platform.WINDOWS -> configDirectory + File.separator + torrServerFileName()
+        Platform.MAC -> configDirectory + File.separator + torrServerFileName()
         else -> throw UnsupportedOperationException("Platform not supported ${platformName()}")
     }
 
@@ -28,6 +28,7 @@ internal actual object TorrserverConfig : ServerConfig {
         return when (platformName()) {
             Platform.LINUX -> fileName
             Platform.WINDOWS -> "$fileName.exe"
+            Platform.MAC -> fileName
             else -> throw UnsupportedOperationException("Platform not supported ${platformName()}")
         }
     }
@@ -37,14 +38,9 @@ internal actual object TorrserverConfig : ServerConfig {
     private fun backUpTorrServerFileName() = "old_${torrServerFileName}"
 
     private fun getConfigDirectory(): String = when (platformName()) {
-        Platform.LINUX -> {
-            val xdgConfigHome = System.getenv("XDG_CONFIG_HOME")
-            Paths.get(xdgConfigHome ?: "${System.getProperty("user.home")}/.config").toString()
-        }
-        Platform.WINDOWS -> {
-            val localAppData = System.getenv("LOCALAPPDATA")
-            Paths.get(localAppData).resolve("MyApp").toString()
-        }
+        Platform.LINUX -> System.getenv("XDG_CONFIG_HOME")
+        Platform.WINDOWS -> System.getenv("LOCALAPPDATA")
+        Platform.MAC -> "${System.getProperty("user.home")}/Library/Application Support"
         else -> throw UnsupportedOperationException("Platform not supported ${platformName()}")
     }
 }
