e6c938360a5ff45d68b4a4797881befd3a3d6b97
dikhawk <dik_hawk@mail.ru>
2025-10-14 20:57:42 +0600

    updated hot reload

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,7 +2,7 @@
 import com.android.build.api.dsl.ManagedVirtualDevice
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.compose.reload.ComposeHotRun
+import org.jetbrains.compose.reload.gradle.ComposeHotRun
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
