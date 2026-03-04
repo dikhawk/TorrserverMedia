ade420c42821bae363bf290aa9e99a45a38cb07e
dik <>
2026-03-04 17:23:47 +0600

    added AppCircularProgressIndicator to widgets

---
+++ core/uikit/src/commonMain/kotlin/com/dik/uikit/widgets/AppCircularProgressIndicator.kt
@@ -0,0 +1,12 @@
+package com.dik.uikit.widgets
+
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppCircularProgressIndicator(
+    modifier: Modifier
+) {
+    CircularProgressIndicator(modifier = modifier)
+}
