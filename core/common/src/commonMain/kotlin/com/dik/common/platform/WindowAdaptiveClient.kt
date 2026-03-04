9815323c4c9449eea87e2b98f03ea510c2ec44f2
dik <>
2026-03-04 16:32:05 +0600

    deleted TorrServerStarterJvmPlatform and window adaptive classes/modules

--- core/common/src/commonMain/kotlin/com/dik/common/platform/WindowAdaptiveClient.kt
+++
@@ -1,9 +0,0 @@
-package com.dik.common.platform
-
-import androidx.compose.material3.adaptive.WindowAdaptiveInfo
-import kotlinx.coroutines.flow.StateFlow
-
-interface WindowAdaptiveClient {
-
-    fun windowAdaptiveFlow(): StateFlow<WindowAdaptiveInfo?>
-}
--- core/common/src/commonMain/kotlin/com/dik/common/platform/WindowAdaptiveObserver.kt
+++
@@ -1,9 +0,0 @@
-package com.dik.common.platform
-
-import androidx.compose.material3.adaptive.WindowAdaptiveInfo
-import kotlinx.coroutines.flow.MutableStateFlow
-
-interface WindowAdaptiveObserver {
-
-    fun windowAdaptiveFlow(): MutableStateFlow<WindowAdaptiveInfo?>
-}
