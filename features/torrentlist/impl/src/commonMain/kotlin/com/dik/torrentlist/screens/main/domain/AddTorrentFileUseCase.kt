b637b1a488144f9210923ec3e972513ba0b2741f
dik <>
2026-03-04 17:22:59 +0600

    refactor(torrentlist): use localized error messages in AddTorrentFileUseCase

--- features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/screens/main/domain/AddTorrentFileUseCase.kt
+++ features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/screens/main/domain/AddTorrentFileUseCase.kt
@@ -2,8 +2,10 @@ package com.dik.torrentlist.screens.main.domain
 
 import com.dik.common.Result
 import com.dik.common.errors.Error
+import com.dik.common.i18n.LocalizationResource
 import com.dik.common.onError
 import com.dik.common.onSuccess
+import com.dik.torrentlist.error.toMessage
 import com.dik.torrentlist.utils.FileUtils
 import com.dik.torrserverapi.model.Torrent
 import com.dik.torrserverapi.server.api.TorrentApi
@@ -12,6 +14,7 @@ internal class AddTorrentFileUseCase(
     private val torrentApi: TorrentApi,
     private val findThumbnailForTorrent: FindPosterUseCase,
     private val fileUtils: FileUtils,
+    private val localization: LocalizationResource,
 ) {
     suspend operator fun invoke(path: String): Result<Torrent, AddTorrentFileErrors> {
         if (!fileUtils.isFileExist(path)) return Result.Error(AddTorrentFileErrors.TorrentNotExist)
@@ -27,7 +30,7 @@ internal class AddTorrentFileUseCase(
 
                     return Result.Success(torrent)
                 }.onError { error ->
-                    return Result.Error(AddTorrentFileErrors.UnknownError(error.toString()))
+                    return Result.Error(AddTorrentFileErrors.UnknownError(error.toMessage(localization)))
                 }
         }
 
