5e6f0741e67df1948abf18bd3165fb81702b8a31
dik <>
2026-03-04 17:22:30 +0600

    feat(torrentlist): add localization mapping for FindPosterErrors

--- features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/error/FeratureErrors.kt
+++ features/torrentlist/impl/src/commonMain/kotlin/com/dik/torrentlist/error/FeratureErrors.kt
@@ -1,17 +1,20 @@
 package com.dik.torrentlist.error
 
 import com.dik.common.i18n.LocalizationResource
+import com.dik.torrentlist.screens.main.domain.FindPosterErrors
 import com.dik.torrserverapi.TorrserverError
 import org.jetbrains.compose.resources.StringResource
 import torrservermedia.features.torrentlist.impl.generated.resources.Res
 import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_no_server_connection
 import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_platform_not_supported
+import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_poster_not_found
 import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_response_return_null
 import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_server_file_not_exist
 import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_server_not_started
+import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_title_is_empty
 import torrservermedia.features.torrentlist.impl.generated.resources.main_error_msg_wrong_server_config
 
-suspend fun TorrserverError.toMessage(localization: LocalizationResource): String {
+internal suspend fun TorrserverError.toMessage(localization: LocalizationResource): String {
     return when (this) {
         TorrserverError.HttpError.ResponseReturnNull ->
             Res.string.main_error_msg_response_return_null.asString(localization)
@@ -27,6 +30,14 @@ suspend fun TorrserverError.toMessage(localization: LocalizationResource): Strin
         is TorrserverError.HttpError.ResponseReturnError -> this.message
         is TorrserverError.Server.WrongConfiguration ->
             Res.string.main_error_msg_wrong_server_config.asString(localization)
+    }
+}
+
+internal suspend fun FindPosterErrors.toMessage(localization: LocalizationResource): String {
+    return when (this) {
+        FindPosterErrors.PosterNotFound -> Res.string.main_error_msg_poster_not_found.asString(localization)
+        FindPosterErrors.TitleIsEmpty -> Res.string.main_error_msg_title_is_empty.asString(localization)
+        is FindPosterErrors.UnknownError -> this.error
     }
 }
 
