f34dc99794c28d9cb0c628dfecb96ba3e660dd4c
dik <>
2026-03-05 15:12:45 +0600

    added plugin stabilityAnalyzer

--- features/torrentlist/impl/build.gradle.kts
+++ features/torrentlist/impl/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.compose)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.stabilityAnalyzer)
 }
 
 android {
