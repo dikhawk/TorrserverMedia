376e2920b66c3041f621a9dbeba86d21485b1f05
dik <>
2026-03-11 15:09:29 +0600

    remove FakePlatformModule.android.kt from androidUnitTest

--- features/torrentlist/impl/src/androidUnitTest/kotlin/com/dik/torrentlist/details/FakePlatformModule.android.kt
+++
@@ -1,9 +0,0 @@
-package com.dik.torrentlist.details
-
-import android.content.Context
-import io.mockk.mockk
-import org.koin.dsl.module
-
-actual fun platformModule() = module {
-    single<Context> { mockk(relaxed = true) }
-}
