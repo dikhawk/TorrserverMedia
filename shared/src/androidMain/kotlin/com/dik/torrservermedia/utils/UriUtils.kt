4e977a83ca0e0026363572af2e4e431796891253
dik <>
2026-03-23 19:51:29 +0600

    added getFileName to Uri and updated copyFileToCache to use it

--- shared/src/androidMain/kotlin/com/dik/torrservermedia/utils/UriUtils.kt
+++ shared/src/androidMain/kotlin/com/dik/torrservermedia/utils/UriUtils.kt
@@ -2,6 +2,7 @@ package com.dik.torrservermedia.utils
 
 import android.content.Context
 import android.net.Uri
+import android.provider.OpenableColumns
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.io.File
@@ -15,8 +16,8 @@ fun Uri.pathToFile(context: Context): String? {
 }
 
 fun Uri.copyFileToCache(context: Context): String? {
-    val file = File(path)
-    val cacheFile = File(context.cacheDir.path + "/${file.name}")
+    val fileName = this.getFileName(context)
+    val cacheFile = File(context.cacheDir.path,fileName)
     BufferedInputStream(context.contentResolver.openInputStream(this)).use { inputStream ->
         BufferedOutputStream(cacheFile.outputStream()).use { outputStream ->
             inputStream.copyTo(outputStream)
@@ -24,4 +25,30 @@ fun Uri.copyFileToCache(context: Context): String? {
     }
 
     return cacheFile.path
+}
+
+fun Uri.getFileName(context: Context): String {
+    var result: String? = null
+
+    if (this.scheme == "content") {
+        val cursor = context.contentResolver.query(this, null, null, null, null)
+        cursor.use { cursor ->
+            if (cursor != null && cursor.moveToFirst()) {
+                val index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (index != -1) {
+                    result = cursor.getString(index)
+                }
+            }
+        }
+    }
+
+    if (result == null) {
+        result = this.path
+        val cut = result?.lastIndexOf('/') ?: -1
+        if (cut != -1) {
+            result = result?.substring(cut + 1)
+        }
+    }
+
+    return Uri.decode(result) ?: "unknown_file_${System.currentTimeMillis()}"
 }
